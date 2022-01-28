b926a40aeb442cdae24ed26241f2d1ce38450dba
BlackPhlox <mlr@redbarnet.dk>
2022-01-28 01:21:03 +0100

    Working version, logic incorrect

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,41 @@
 use bevy::{prelude::*, render::camera::{ActiveCameras, CameraPlugin}};
 
+use strum::IntoEnumIterator;
+use strum_macros::EnumIter;
+
+#[macro_export]
+macro_rules! next_enum {
+    ($l:ident, $k:expr) => {
+        $l::iter()
+            .enumerate()
+            .nth(
+                $l::iter()
+                    .enumerate()
+                    .find(|a| a.1 == *$k.current())
+                    .map(|(i, _)| {
+                        if i + 1 > $l::iter().count() - 1 {
+                            0usize
+                        } else {
+                            i + 1
+                        }
+                    })
+                    .unwrap(),
+            )
+            .unwrap()
+            .1
+    };
+}
+
 fn main() {
     App::new()
         .insert_resource(Msaa { samples: 4 })
         .add_plugins(DefaultPlugins)
-        .add_state(CameraState::Camera1)
+        .add_state(CameraState::Camera2)
         .add_startup_system(setup)
         .add_system(switch_cameras)
+        .add_system(switch_cam_state)
+        .add_system(input)
         .run();
-}
-
-#[derive(Clone, Eq, PartialEq, Debug, Hash)]
-enum CameraState {
-    Camera1,
-    Camera2,
 }
 
 /// set up a simple 3D scene
@@ -21,6 +43,8 @@ fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
+    mut active_cameras: ResMut<ActiveCameras>,
+    mut cam_state: ResMut<State<CameraState>>,
 ) {
     // plane
     commands.spawn_bundle(PbrBundle {
@@ -46,75 +70,67 @@ fn setup(
     commands.spawn_bundle(PerspectiveCameraBundle {
         transform: Transform::from_xyz(-2.0, 5.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..Default::default()
-    }).insert(Camera1);
+    }).insert(CameraA(CameraState::Camera1));
 
     //Defaults to the latest added camera
     commands.spawn_bundle(PerspectiveCameraBundle {
         transform: Transform::from_xyz(0.0, 5.0, -5.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..Default::default()
-    }).insert(Camera2);
+    }).insert(CameraA(CameraState::Camera2));
+
+    active_cameras.add(format!("{:?}", cam_state.current()).as_str());
+}
+
+#[derive(Clone, Eq, PartialEq, Debug, Hash, EnumIter)]
+enum CameraState {
+    Camera1,
+    Camera2,
 }
 
 #[derive(Component)]
-struct Camera1;
-#[derive(Component)]
-struct Camera2;
+struct CameraA(CameraState);
 
+fn switch_cam_state(
+    mut cam_state: ResMut<State<CameraState>>,
+    keys: Res<Input<KeyCode>>,
+){
+    if keys.just_pressed(KeyCode::C) {
+        let n_state = next_enum!(CameraState, cam_state);
+        let _ = cam_state.set(n_state);
+    }
+}
 
 fn switch_cameras(
     mut active_cameras: ResMut<ActiveCameras>,
-    mut cam_state: ResMut<State<CameraState>>,
+    cam_state: Res<State<CameraState>>,
     keys: Res<Input<KeyCode>>,
-    mut query: QuerySet<(
-        QueryState<(&Camera1, &mut Camera)>,
-        QueryState<(&Camera2, &mut Camera)>,
-    )>,
-    mut query1: Query<&mut Camera>
+    mut query: Query<(&mut CameraA, &Camera)>
 ) {
-    let q0 = query.q0().single().1;
-    let q1 = query.q1().single().1;
-    
     if keys.just_pressed(KeyCode::C) {
-        if cam_state.current().eq(&CameraState::Camera1){
-            let removed_cam = active_cameras.remove(CameraPlugin::CAMERA_3D);
-            
-            if removed_cam.is_some() {
-                let r = removed_cam.unwrap();
-                if r.entity.is_some() {
-                    let t = removed_cam.unwrap().entity;
-                    query1.get();
+        if let Some (ac) = active_cameras.get(CameraPlugin::CAMERA_3D) {
+            if let Some (o) = ac.entity {
+                if let Ok(p) = query.get(o) {
+                    let s = &cam_state.current();
+                    if p.0.0.ne(s) {
+                        active_cameras.remove(CameraPlugin::CAMERA_3D);
+                        if let Some (q) = query.iter_mut().filter(|f| f.0.0.eq(s)).last() {
+                            active_cameras.add(CameraPlugin::CAMERA_3D);
+                        }
+                    }
                 }
-            }
-            
-            removed_cam.
-            active_cameras.add(CameraPlugin::CAMERA_3D);
+            }   
         }
     }
-    /*
-    if settings.locked_to_player && !settings.ltp {
-        act_cams.remove("Camera3d");
+}
 
-        let (_, mut b) = query.q1_mut().single_mut().unwrap();
-        b.name = Some("Camera3d".to_string());
-
-        act_cams.add("Camera3d");
-
-        let (_, mut b) = query.q0_mut().single_mut().unwrap();
-        b.name = Some("Test".to_string());
-
-        settings.ltp = true;
-    } else if !settings.locked_to_player && settings.ltp {
-        act_cams.remove("Camera3d");
-
-        let (_, mut b) = query.q0_mut().single_mut().unwrap();
-        b.name = Some("Camera3d".to_string());
-
-        act_cams.add("Camera3d");
-
-        let (_, mut b) = query.q1_mut().single_mut().unwrap();
-        b.name = Some("Test".to_string());
-
-        settings.ltp = false;
+fn input(
+    active_cameras: Res<ActiveCameras>,
+    keys: Res<Input<KeyCode>>,
+) {
+    if keys.just_pressed(KeyCode::C) {
+        println!("");    
+        for active_cam in active_cameras.iter() {
+            println!("{:?}", active_cam);
+        }
     }
-    */
 }
